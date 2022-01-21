88555f936a5ce718b3af1489531b2f30a4f965bd
Sailornina <sylcardozo.sc@gmail.com>
2022-01-21 17:33:17 +0100

    about and skills in progress

---
+++ src/components/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+import '../styles/styles.scss';
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
+import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
+
+function About() {
+        return (
+        <section id="about"> 
+        <div>
+            <h1>About me:</h1>
+            <hr />
+          <div className="about">
+              <strong>Hi, I'm Antonella!</strong><br /><br />
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br /><br />
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br /><br />
+              </p>
+           </div>
+           <div className="links.work">
+            <a
+              href="#contact"
+              className="link.contact">
+              Work With Me
+            </a><br /><br />
+            <a
+              href="#projects"
+              className="link.projects">
+              See My Personal Projects
+            </a>
+          </div>
+          <div>
+          <FaGithub />
+          <FaLinkedin />
+          <FaTwitter />
+          </div>
+        </div>  
+        </section>      
+        );
+    }
+
+
+export default About;
---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaCalendar } from '@react-icons/all-files/fa/FaCalendar'
+
+function Skills() {
+    return (
+        <div>
+      <FaCalendar />
+        </div>
+    )
+};
+
+export default Skills;
