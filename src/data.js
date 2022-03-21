54d60438dcfa26a1244ebc4daadd04b0ff5e88dc
Sailornina <sylcardozo.sc@gmail.com>
2022-03-21 23:39:09 +0100

    third project was added

--- src/data.js
+++ src/data.js
@@ -15,12 +15,12 @@ export const projects = [
     image: `${process.env.PUBLIC_URL}/image/project_2.gif`,
     link: "https://sailornina.github.io/odin-library/",
   },
-  /*{
+  {
     title: "Technical Documentation Page",
     subtitle: "HTML and CSS",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    //image: ,//
+    image: `${process.env.PUBLIC_URL}/image/project_3.gif`,
     link: "https://sailornina.github.io/technical-documentation-page/",
-  }*/
+  }
 ]
