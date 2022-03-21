087819d071fce6bfd9aea2d333d1a7e1b2fa5f51
Sailornina <sylcardozo.sc@gmail.com>
2022-03-21 23:10:58 +0100

    second project was added

--- src/data.js
+++ src/data.js
@@ -7,15 +7,15 @@ export const projects = [
     image: `${process.env.PUBLIC_URL}/image/project_1.gif`,
     link: "https://cmyk-lime.vercel.app/",
   },
-  /*{
+  {
     title: "Library",
     subtitle: "JavaScript Vanilla",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    //image: ,//
+    image: `${process.env.PUBLIC_URL}/image/project_2.gif`,
     link: "https://sailornina.github.io/odin-library/",
-  },/*
-  {
+  },
+  /*{
     title: "Technical Documentation Page",
     subtitle: "HTML and CSS",
     description:
