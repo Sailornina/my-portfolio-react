a6e7a8810a0f14691e2309a618f7fcf604b5c5ce
Sailornina <sylcardozo.sc@gmail.com>
2022-03-24 17:16:49 +0100

    Inserted the Google Maps

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,12 @@ function Contact() {
   return (
     <section id="contact">
       <div className="contact-me">
-        <div className="personal-data">
+        <div className="map-data">
+          <iframe
+            title="stockholm"
+            style="height:100%; width:100%; border:0;"
+            frameborder="0"
+            src="https://www.google.com/maps/embed/v1/place?q=stockholm&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" />
 
         </div>
       </div>
