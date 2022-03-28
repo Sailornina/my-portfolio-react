1ae2dcdb63ec9d6a4a8ca01e1f995e89e8f7c505
Sailornina <sylcardozo.sc@gmail.com>
2022-03-28 18:11:45 +0200

    form was added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <section id="contact">
       <div className="contact-me">
-      <h1>Contacme</h1>
+        <h1>Contact me</h1>
         <div className="map-data">
           <iframe
             title="stockholm"
@@ -13,29 +13,48 @@ function Contact() {
             src="https://www.google.com/maps/embed/v1/place?q=stockholm&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
         </div>
+      </div>
+      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+        <h2 className="title-form">Hire me!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div className="info-form">
+          <label htmlFor="name">
+            Name
+          </label>
+          <imput
+            type="text"
+            id="name"
+            name="name"
+          />
         </div>
-        <div>
-          <div className="addres-contact">
-            <h2>
-              Addres</h2>
-            <p>
-              Stockholm <br />
-              Sweden
-            </p>
-          </div>
-          <div className="mail-contact">
-            <div>
-              <h2>Email</h2>
-              <a className="mail-contact-exemple">
-                sylcardozo.sc@gmail.com
-              </a>
-            </div>
-          </div>
+        <div className="info-form">
+          <label htmlFor="email">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+          />
         </div>
+        <div className="info-form">
+          <label
+            htmlFor="message">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Submit
+        </button>
+      </form>
     </section>
-
   );
-
 }
 
 export default Contact;
