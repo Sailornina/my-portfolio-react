8dcd4bddd3374e352621bfbe8e8b98d1a532206a
Sailornina <sylcardozo.sc@gmail.com>
2022-04-14 19:15:12 +0200

    contact styles in progress

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,70 +14,66 @@ function Contact() {
 
   };
 
-
   return (
     <section id="contact">
       <div className="contact-container">
-      <div className="contact-me">
-        <h1>Contact me</h1>
-        <div className="map-data">
-          <iframe
-            title="stockholm"
-            frameborder="0"
-            src="https://www.google.com/maps/embed/v1/place?q=stockholm&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-          />
+        <div className="contact-me">
+          <h1>Contact me</h1>
+          <div className="map-data">
+            <iframe
+              title="stockholm"
+              frameborder="0"
+              src="https://www.google.com/maps/embed/v1/place?q=stockholm&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            />
+          </div>
         </div>
-      </div>
-      <form onSubmit={handleSubmit} name="simple-contact-form" data-netlify="true" data-netlify-honeypot="bot-field" className="form-container">
-        <h2 className="title-form">Hire me!</h2>
-        <p>Here is my contact information, in case you want to have some coffee and discuss any of my projects.</p>
-        <div className="info-contact-form">
-          <label htmlFor="name">
-            Name
-          </label>
-          <input
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-            value={formData.name}
-            type="text"
-            id="name"
-            name="name"
-            className="form-control"
-          />
-        </div>
-        <div className="info-contact-form">
-          <label htmlFor="email">
-            Email
-          </label>
-          <input
-            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-            value={formData.email}
-            type="email"
-            id="email"
-            name="email"
-            className="form-control"
-          />
-        </div>
-        <div className="info-contact-form">
-          <label
-            htmlFor="message">
-            Message
-          </label>
-          <textarea
-            onChange={(e) => setFormData({ ...formData, message: e.target.value })}
-            value={formData.message}
-            id="message"
-            name="message"
-            className="form-control"
-          />
-        </div>
-        <button
-          type="submit" className="button">
-          Get in Touch
-        </button>
-      </form>
-      <div>
-      <button class="back-to-top">Back to Top</button>
-      </div>
+        <form onSubmit={handleSubmit} name="simple-contact-form" data-netlify="true" data-netlify-honeypot="bot-field" className="form-container">
+          <h2 className="title-form">Hire me!</h2>
+          <p>Here is my contact information, in case you want to have some coffee and discuss any of my projects.</p>
+          <div className="info-contact-form">
+            <label htmlFor="name">
+              Name
+            </label>
+            <input
+              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+              value={formData.name}
+              type="text"
+              id="name"
+              name="name"
+              className="form-control"
+            />
+          </div>
+          <div className="info-contact-form">
+            <label htmlFor="email">
+              Email
+            </label>
+            <input
+              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+              value={formData.email}
+              type="email"
+              id="email"
+              name="email"
+              className="form-control"
+            />
+          </div>
+          <div className="info-contact-form">
+            <label
+              htmlFor="message">
+              Message
+            </label>
+            <textarea
+              onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+              value={formData.message}
+              id="message"
+              name="message"
+              className="form-control"
+            />
+          </div>
+          <button
+            type="submit" className="button">
+            Get in Touch
+          </button>
+        </form>
       </div>
     </section>
   );
