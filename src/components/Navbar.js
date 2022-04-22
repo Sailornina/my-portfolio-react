851c3fd594a7dfa92f85c2b35622af4be87cd9aa
Sailornina <sylcardozo.sc@gmail.com>
2022-04-22 12:52:06 +0200

    navbar in progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,11 @@ function Navbar() {
     return (
         <header id="navbar">
             <div className="my-name-navbar">
-                <a href="#about" className="navbar-name">Antonella Cardozo</a>
              <nav>
                 <a href="#projects" className="navbar-projects">My projects</a>
                 <a href="#skills" className="navbar-skills">Skills</a>
+                <a href="#contact" className="navbar-contact">Contact me</a>
              </nav>
-             <a href="#contact" className="navbar-contact">Contact me</a>
             </div>
         </header>
     );
